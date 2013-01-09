f6ffb7e25fd3c10df15b4f17b9b3c41785c3cdf8
Stefan Nagey <stefan@nagey.com>
2013-01-09 16:36:13 -0500

    utility functions to generate the repository URL

--- qorvis-base.rb
+++ qorvis-base.rb
@@ -1,3 +1,11 @@
+def orgname
+  "QorvisDigital"
+end
+
+def github_repo_url(reponame)
+  "git@github.com:#{orgname}/#{reponame}.git"
+end
+
 def password_gen(size=12)
   chars = (('a'..'z').to_a + ('0'..'9').to_a) - %w(i o 0 1 l 0)  
   (1..size).collect{|a| chars[rand(chars.size)] }.join
