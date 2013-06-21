b2902392bbf7f25370b9b7f6a42aad950ddeb399
Stefan Nagey <stefan@nagey.com>
2013-06-20 23:15:04 -0400

    added depts to install & build 15sfest

--- 15sfest.rb
+++ 15sfest.rb
@@ -1,3 +1,16 @@
+def webdir
+  "/var/www/html"
+end
+
+def appdir
+  "/var/www/app"
+end
+
+def srcdir
+  "~/src/15sfest"
+end
+
+
 dep '15sfestWeb' do
   web_hostname = "15sfest.com"
   listen_port = 80
@@ -6,7 +19,7 @@ dep '15sfestWeb' do
     "nagey:vhost enabled.nginx".with(
       :domain => web_hostname,
       :vhost_type => "websocket_proxy",
-      :path => "/var/www",
+      :path => webdir,
       :nginx_prefix => "/usr/local/nginx",
       :domain_aliases => '',
       :force_https => 'no',
@@ -16,11 +29,69 @@ dep '15sfestWeb' do
       :proxy_port => 3000,
       :proxy_host => "localhost"
     ),
-    "nagey:running.nginx".with(:nginx_prefix => "/usr/local/nginx")
+    "nagey:running.nginx".with(:nginx_prefix => "/usr/local/nginx"),
+    "15sfest build"
   ]
 end
 
 dep "hostname configured", :myhostname do
   met? { shell?("cat /etc/hosts|grep #{myhostname}") || shell?("host #{myhostname}") }
   meet { sudo "echo '127.0.0.1 #{myhostname}' >> /etc/hosts" }
+end
+
+dep "15sfest cloned" do
+  requires "15sfest-srcdir", "15sfest-appdir", "15sfest-gitdir", "git"
+  
+  git_dir = gitdir
+  git_url = "git@github.com:nagey/ouiff.git"
+  
+  met? { File.exists? "#{git_dir}/.git/config" }
+  meet do
+    shell "git clone #{git_url} #{git_dir}"
+  end
+end
+
+dep "15sfest build" do
+  requires "15sfest cloned", "15sfest build-web", "15sfest build-app"
+end
+
+dep "15sfest build-web" do
+  requires "grunt", "bower", "npm"
+  shell "cd #{srcdir}/html; grunt && rm -rf #{webdir}/* && cp -pr #{srcdir}/html/dist #{webdir}"
+end
+
+dep "15sfest build-app" do
+  requires "npm", "nodejs.src"
+  sudo "cd #{srcdir}/app; killall -9 node; cp -pr #{srcdir}/app/* #{appdir}; npm start"
+end
+
+dep "npm" do
+  requires "nodejs.src"
+end
+
+dep "grunt" do
+  requires "npm"
+  met? { shell? "which grunt" }
+  meet { sudo "npm install -g grunt-cli" }
+end
+
+dep "bower" do
+  requires "npm"
+  met? { shell? "which bower" }
+  meet { sudo "npm install -g bower" }
+end
+
+dep '15sfest-srcdir' do
+  met? { File.exists? webdir }
+  meet { sudo "mkdir -p #{webdir}" }
+end
+
+dep '15sfest-appdir' do
+  met? { File.exists? appdir }
+  meet { sudo "mkdir -p #{appdir}" }
+end
+
+dep '15sfest-gitdir' do
+  met? { File.exists? srcdir }
+  meet { sudo "mkdir -p #{srcdir}" }
 end
